d963fd282247e0743cf7a30fbafb25178a5b6c41
TheRealAGBOOLA <agboolasamson602@gmail.com>
2023-11-30 16:56:06 +0100

    some media queries and some more keyboard accessibility

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ let profileMenu = document.getElementById("profile");
 let user = document.getElementById("user");
 let popUpmenu = document.getElementById("userTab");
 
+//display the menu and alert interchangeably
 alerts.addEventListener("click", () => {
-    if (alertBox.style.display === "block") {
-        alertBox.style.display = "none";
-    } else {
-        alertBox.style.display = "block";
-        profileMenu.style.display = "none";
-    }
+  if (alertBox.style.display === "block") {
+    alertBox.style.display = "none";
+  } else {
+    alertBox.style.display = "block";
+    profileMenu.style.display = "none";
+  }
 });
 
 user.addEventListener("click", () => {
-    if (profileMenu.style.display === "block") {
-        profileMenu.style.display = "none";
-    } else {
-        profileMenu.style.display = "block";
-        alertBox.style.display = "none";
-    }
+  if (profileMenu.style.display === "block") {
+    profileMenu.style.display = "none";
+  } else {
+    profileMenu.style.display = "block";
+    profileMenu.firstElementChild.firstElementChild.nextElementSibling.focus();
+
+    alertBox.style.display = "none";
+  }
 });
+// focus on the first element in the user profile menu
