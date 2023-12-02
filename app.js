5d880d3a9e848cfa628a9d8cb33b9381f2a17d28
TheRealAGBOOLA <agboolasamson602@gmail.com>
2023-12-02 08:07:35 +0100

    added the plan adverts

--- app.js
+++ app.js
@@ -3,25 +3,32 @@ let alertBox = document.getElementById("alerts");
 let profileMenu = document.getElementById("profile");
 let user = document.getElementById("user");
 let popUpmenu = document.getElementById("userTab");
+let planAd = document.getElementById("planAd");
+let removeAd = document.getElementById("remove-ad");
 
 //display the menu and alert interchangeably
 alerts.addEventListener("click", () => {
-  if (alertBox.style.display === "block") {
-    alertBox.style.display = "none";
-  } else {
-    alertBox.style.display = "block";
-    profileMenu.style.display = "none";
-  }
+    if (alertBox.style.display === "block") {
+        alertBox.style.display = "none";
+    } else {
+        alertBox.style.display = "block";
+        profileMenu.style.display = "none";
+    }
 });
 
 user.addEventListener("click", () => {
-  if (profileMenu.style.display === "block") {
-    profileMenu.style.display = "none";
-  } else {
-    profileMenu.style.display = "block";
-    profileMenu.firstElementChild.firstElementChild.nextElementSibling.focus();
+    if (profileMenu.style.display === "block") {
+        profileMenu.style.display = "none";
+    } else {
+        profileMenu.style.display = "block";
+        profileMenu.firstElementChild.firstElementChild.nextElementSibling.focus();
 
-    alertBox.style.display = "none";
-  }
+        alertBox.style.display = "none";
+    }
 });
-// focus on the first element in the user profile menu
+
+removeAd.addEventListener("click", () => {
+    planAd.style.display = "none";
+});
+
+//TODO:add keyboard accessibility to the profilemenu
